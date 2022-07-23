16b0f039ea87e46056d652f0f076fac1e9e92575
Leonardo de Albuquerque Gouveia <leogouv@gmail.com>
2022-07-23 17:03:49 -0300

    Add eslint to node-express

--- node-express/routes/index.js
+++ node-express/routes/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Client = require('../models/Client');
+const Client = require("../models/Client");
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
+router.get("/", async function (req, res, next) {
   const clients = await Client.findAll({ limit: 10 });
-  return res.json(clients)
+  return res.json(clients);
 });
 
-router.get('/hello', (req, res, next) => {
-  res.render('index', { title: 'Express' });
-})
+router.get("/hello", (req, res, next) => {
+  res.render("index", { title: "Express" });
+});
 module.exports = router;
